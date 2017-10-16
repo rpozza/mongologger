a9177a83b5a64701894a3013dab8cd98034ee74d
Riccardo Pozza <r.pozza@surrey.ac.uk>
2017-10-16 11:49:46 +0000

    Modified Install

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 sudo mkdir /usr/local/bin/mongologger
 sudo cp rabbitmq-mongodb.py /usr/local/bin/mongologger/rabbitmq-mongodb.py
 sudo cp myconfig.json /usr/local/bin/mongologger/myconfig.json
-sudo cp rabbitmq-mongodb.service /lib/systemd/system/rabbitmq-mongodb.service
-sudo chmod 644 /lib/systemd/system/rabbitmq-mongodb.service
+sudo cp rabbitmq-mongodb@.service /lib/systemd/system/rabbitmq-mongodb@.service
+sudo chmod 644 /lib/systemd/system/rabbitmq-mongodb@.service
 sudo systemctl daemon-reload 
-sudo systemctl enable rabbitmq-mongodb.service
+#sudo systemctl enable rabbitmq-mongodb@{1..10}.service
 
-#reboot and systemctl status rabbitmq-objstore.service
+#reboot and systemctl status rabbitmq-mongodb@{1..10}.service
 
